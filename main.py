7de30d8f070b92aab329f7fb1325915b354f79a6
GHOUL Nadir <nadir.ghoul@hotmail.com>
2018-11-08 14:14:32 +0100

    First successful attempt, but with dbm.dumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """WARNING: AT THIS STAGE, THIS LIBRARY IS NOR THREAD SAFE, NOR PROCESS SAFE"""
 # import cryptography
-import os, unittest, random
+import os, unittest, random, math, dbm.dumb
 
 USE_GETPASS = True
 
@@ -37,7 +37,9 @@ def encrypt_data(key, data) -> bytes:
 	encryptor, padder = cipher.encryptor(), PKCS7(ENCRYPTION_ALGORITHM.block_size).padder()
 	return nonce + encryptor.update(padder.update(data) + padder.finalize()) + encryptor.finalize()
 
-
+# For the moment, deciphering with a wrong key is like accessing an non reserved
+# 	memory adress, sometimes it crashes, sometimes it passes (in this case it
+# 	fails to unpad the deciphered data)
 def decrypt_data(key, data) -> bytes:
 	# Encrypt using the configuration above, later, will return config along
 	# 	with data, for now, it return None as a config element, because
@@ -54,6 +56,8 @@ def decrypt_data(key, data) -> bytes:
 BLOCK_INDEX_LENGTH = 4
 DEFAULT_NAME = 'default'
 DEFAULT_TMP_NONCE_LENGTH = 8
+BLOCK_SIZE = 1024
+CHUNK_SIZE = 1024*1024
 
 
 # TODO: Create recovery methods, and store them at the end of blocks
@@ -124,17 +128,42 @@ def generate_key(name, key_name='DEFAULT'):
 		finally:
 			os.chdir('..')
 
+def fetch_key(name, key_name='default'):
+	"""Ask the password and return the key"""
+	# TODO: Fix this big hole to become thread safe
+	key_name = key_name.lower()
+	assert isinstance(name, str)
+	assert isinstance(key_name, str)
+	assert os.path.exists(name)
+	assert os.path.exists(os.path.join(name, "keyring", key_name))
+	with open(os.path.join(name, "keyring", key_name), 'rb') as f:
+		encrypted_key = f.read()
+	message0 = message = 'Please enter the %r key of %r' % (key_name, name)
+	message1 = 'Error, password incorrect, Please retry.\n' + message0
+	decrypted_key = b''
+	while not decrypted_key.endswith(b' is the right key'):
+		try:
+			decrypted_key = decrypt_data(getpass.getpass(message).encode().zfill(
+				ENCRYPTION_KEY_SIZE // 8)[:ENCRYPTION_KEY_SIZE // 8], encrypted_key)
+		except ValueError:
+			decrypted_key = b'Nope'
+		if not decrypted_key.endswith(b' is the right key'):
+			message = message1
+	kkk = len(b' is the right key')
+	print(decrypted_key[:-kkk])
+	return decrypted_key[:-kkk]
 
-def init(name=DEFAULT_NAME, block_size=1024 * 1024, noise_as_background=True):
+def init(name=DEFAULT_NAME, chunk_size=CHUNK_SIZE, noise_as_background=True):
 	"""Initialize a local folder, with a fixed block of noise or zeros"""
 	# TODO: Decide if it is a good idea to restrain block sizes to fit with
 	# 	aes block size
 	# Will raise an exception if the folder already exists
 	os.makedirs(name)
+	assert chunk_size == CHUNK_SIZE
 
 	with open(os.path.join(name, os.path.join('BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH))), 'wb') as f:
 		# TODO: Optimize to make it chunk by chunk for big block sizes
-		f.write(os.urandom(block_size) if noise_as_background else bytes(block_size))
+		f.write(os.urandom(chunk_size) if noise_as_background else bytes(block_size))
 
 	generate_key(name, 'default')
 
@@ -156,8 +185,43 @@ def nuke(name):
 	os.rmdir(os.path.join(name, 'keyring'))
 	os.rmdir(name)
 
+def read(name, path):
+	# TODO: Checks
+	with dbm.dumb.open(os.path.join(name, 'BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH))) as db:
+		return decrypt_data(fetch_key(name), db[path])
+
+def write(name, path, content):
+	# TODO: Checks
+	with dbm.dumb.open(os.path.join(name, 'BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH))) as db:
+		db[path] = encrypt_data(fetch_key(name), content)
+
+	"""assert isinstance(content, bytes)
+	length = len(content)
+	size = length * 8
+	number_of_blocks = math.ceil(size / BLOCK_SIZE)
+	with open(os.path.join(name, 'bitmap'), 'rb') as f:
+		bitmap = f.read()"""
+
+	"""return
+	# TODO: Go deeper and search free bits.
+	# Will raise ValueError if it doesn't find anything
+	# TODO: Make data more sparse
+	# TODO: Automatically add blocs, and warn if too much of them
+	index = bitmap.index(bytes(number_of_blocks // 8)) # 1 byte -> 8 blocks
+	with open(os.path.join(name, os.path.join('BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH))), 'ab') as f:
+		pass
+	# TODO: Make sure we don't overlap
+	with open(os.path.join(name, os.path.join('BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH))), 'ab') as f:
+		# TODO: Optimize to make it chunk by chunk for big block sizes
+		f.seek(index)
+		f.write(encrypted)"""
+
 
 class Tests(unittest.TestCase):
+
+	# TODO: Use random values to initialize database key, to not have to silent
+	# 	those tests
+	@unittest.skip("Skipping it while we don't change the initialization function")
 	def test_initialization(self):
 		# First of all, we choose an unique name to avoid messing with actual data
 
@@ -172,6 +236,33 @@ class Tests(unittest.TestCase):
 				self.assertEqual(len(data), block_size, len(data))
 				if not noise:
 					self.assertEqual(set(data), {0}, set(data))
+		finally:
+			# I don't want to left trash
+			print(os.path.abspath('.'))
+			nuke(name)
+			print('Database %s deleted' % name)
+
+	def test(self):
+		# First of all, we choose an unique name to avoid messing with actual data
+		noise = random.choice((False, True))
+		name = 'tmp.%s' % hex(random.randint(0, 16 ** DEFAULT_TMP_NONCE_LENGTH))[2:].zfill(DEFAULT_TMP_NONCE_LENGTH)
+		block_size = 1024 * 1024
+		try:
+			init(name, block_size, noise)
+			print('Database initialized in %s' % name)
+			with open(os.path.join(name, 'BLOCK%s' % str(0).zfill(BLOCK_INDEX_LENGTH)), 'rb') as f:
+				data = f.read()
+				self.assertEqual(len(data), block_size, len(data))
+				if not noise:
+					self.assertEqual(set(data), {0}, set(data))
+
+			text = input('Enter the text you want to save').encode()
+			write(name, 'main.txt', text)
+			r = read(name, 'main.txt')
+			print(r)
+			assert r == text
+
+
 		finally:
 			# I don't want to left trash
 			print(os.path.abspath('.'))
